1a97e6197638295c235fab9d988acc175bdbb9d6
Qian Tang <q.tang6@student.unimelb.edu.au>
2020-12-15 20:53:13 +1100

    create the first modal

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+  <p>Welcome</p>
+  <div v-if="showModal">
+    <Modal :header="header" v-bind:text="text" theme="" @close ="toggleModal"  >
+      <template v-slot:links>
+        <a href="#">Sign up</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>Ninja Giveaway</h1>
+      <p>Grab your ninja swag for half price</p>
+    </Modal>
+  </div>
+  <button v-on:click.alt="toggleModal">Open Modal(alt)</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data(){
+    return {
+      title:"Hello",
+      header:"Sign up for the Giveway",
+      text:"Grab your ninja swag for half price",
+      showModal:false
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.boo)
+      this.$refs.boo.classList.add('activr')
+      this.$refs.boo.focus()
+    },
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
   }
 }
 </script>
